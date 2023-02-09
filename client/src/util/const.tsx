0618abb5e4a5ca932240813f647ff289e0baa8e6
eisandbar <eisandbar@gmail.com>
2023-02-09 02:05:31 -0500

    build(client): Change server host
    Changed server host to server.ytlive.online

--- client/src/util/const.tsx
+++ client/src/util/const.tsx
@@ -1,3 +1,3 @@
-export const host = "ytlive.online:3322";
+export const host = "https://server.ytlive.online";
 export const streamMinWidth: number = 16 * 16;
 export const categoryMinWidth: number = 10 * 16;
