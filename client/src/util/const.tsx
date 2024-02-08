f12087091b816dd5db97d944393c5ea1c3cb8392
eisandbar <eisandbar@gmail.com>
2024-02-08 16:42:14 -0500

    fix(client): Fix host link
    Updated host ip to proper name

--- client/src/util/const.tsx
+++ client/src/util/const.tsx
@@ -1,3 +1,3 @@
-export const host = "https://server.ytlive.online";
+export const host = "https://server.ytlive.eisandbar.xyz";
 export const streamMinWidth: number = 12 * 16;
 export const categoryMinWidth: number = 7 * 16;
